5ed261a7eb49849125aecf988b01a066df017b5a
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-08-13 15:30:17 +0200

    trying to pass actions

--- src/test/java/com/vitruv/methodologist/MethodologistApplicationTests.java
+++ src/test/java/com/vitruv/methodologist/MethodologistApplicationTests.java
@@ -1,9 +1,14 @@
 package com.vitruv.methodologist;
 
+import com.vitruv.methodologist.config.TestSecurityConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestSecurityConfiguration.class)
 class MethodologistApplicationTests {
 	@Test
 	void contextLoads() {}
