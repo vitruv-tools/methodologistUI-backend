3dfda8501ec798593f8be4042dcfcc7146d1243a
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-08-15 09:32:42 +0200

    Fix the Strange request from git hub action again

--- src/main/java/com/vitruv/methodologist/user/controller/dto/request/UserPutRequest.java
+++ src/main/java/com/vitruv/methodologist/user/controller/dto/request/UserPutRequest.java
@@ -3,9 +3,9 @@ package com.vitruv.methodologist.user.controller.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
