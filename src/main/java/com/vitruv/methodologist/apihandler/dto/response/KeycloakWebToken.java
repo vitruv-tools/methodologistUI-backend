495f063fdd612166f13441081588b87ede7a46a0
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-08-20 15:55:34 +0200

    some refactoring

--- src/main/java/com/vitruv/methodologist/apihandler/dto/response/KeycloakWebToken.java
+++ src/main/java/com/vitruv/methodologist/apihandler/dto/response/KeycloakWebToken.java
@@ -9,20 +9,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Data Transfer Object (DTO) representing a Keycloak authentication token response.
+ * Maps the JSON response from Keycloak's token endpoint to a Java object.
+ * Uses snake case naming strategy for JSON property mapping.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-
 public class KeycloakWebToken implements Serializable {
-    private String  accessToken;
-    private String  refreshToken;
-    private Integer  expiresIn;
-    private Integer refreshExpiresIn;
-    private String  tokenType;
-    @JsonProperty("not-before-policy")
-    private Integer  notBeforePolicy;
-    private String  sessionState;
-    private String  scope;
+  private String accessToken;
+  private String refreshToken;
+  private Integer expiresIn;
+  private Integer refreshExpiresIn;
+  private String tokenType;
+
+  @JsonProperty("not-before-policy")
+  private Integer notBeforePolicy;
+
+  private String sessionState;
+  private String scope;
 }
