29b2f2909c6ff05ee9095b704db5b31036d20db5
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-08-22 14:40:11 +0200

    added plugin for formatting and checking

--- src/main/java/tools/vitruv/methodologist/general/controller/responsedto/LatestVersionResponse.java
+++ src/main/java/tools/vitruv/methodologist/general/controller/responsedto/LatestVersionResponse.java
@@ -7,9 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * Data Transfer Object (DTO) representing the latest version information of the application.
- * This response is typically returned by version-check endpoints to inform clients
- * about the current version and whether a forced update is required.
+ * Data Transfer Object (DTO) representing the latest version information of the application. This
+ * response is typically returned by version-check endpoints to inform clients about the current
+ * version and whether a forced update is required.
  */
 @Builder
 @Getter
