6ec34b81a1b29225b081da5e6744adebd928f08b
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2025-09-17 15:18:43 +0200

    rename exception

--- app/src/main/java/tools/vitruv/methodologist/exception/AlreadyExistsFileException.java
+++ app/src/main/java/tools/vitruv/methodologist/exception/AlreadyExistsFileException.java
@@ -4,11 +4,11 @@ package tools.vitruv.methodologist.exception;
  * Exception thrown when a user attempts to upload a file that is already stored in the system.
  * This0 exception is used to prevent duplicate file storage for the same user.
  */
-public class UserFileIsRepeatedException extends RuntimeException {
+public class AlreadyExistsFileException extends RuntimeException {
   public static final String messageTemplate = "This file used before by user!";
 
   /** Constructs a new UserFileIsRepeatedException with no detail message. */
-  public UserFileIsRepeatedException() {
+  public AlreadyExistsFileException() {
     super(messageTemplate);
   }
 }
