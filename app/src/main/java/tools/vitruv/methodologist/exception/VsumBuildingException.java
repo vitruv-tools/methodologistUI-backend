24f8b18ced2583c37422249833222ffa6467b631
Mohammadali Mirzaei <93537795+ma-mirzaei@users.noreply.github.com>
2026-02-09 22:01:29 +0100

    feat: add custom error message for VsumBuildingException in CLI error handling

--- app/src/main/java/tools/vitruv/methodologist/exception/VsumBuildingException.java
+++ app/src/main/java/tools/vitruv/methodologist/exception/VsumBuildingException.java
@@ -7,6 +7,8 @@ package tools.vitruv.methodologist.exception;
  * used as the exception message.
  */
 public class VsumBuildingException extends RuntimeException {
+  public static final String ERROR_TEMPLATE =
+      "Error occurred during maven build! Please fix your setup accordingly!.";
 
   /**
    * Constructs a new {@code VsumBuilingException} with the specified detail message.
