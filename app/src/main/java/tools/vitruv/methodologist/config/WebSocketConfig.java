61125a6da82197e19a47ae9f6d858a90249f9712
Max Oesterle <maxoesterle.kit@gmail.com>
2026-09-13 17:02:09 +0200

    increase WebSocket buffer size for LSP connections to prevent overflow on large reactions

--- app/src/main/java/tools/vitruv/methodologist/config/WebSocketConfig.java
+++ app/src/main/java/tools/vitruv/methodologist/config/WebSocketConfig.java
@@ -1,9 +1,12 @@
 package tools.vitruv.methodologist.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /** Configuration class for setting up WebSocket endpoints. */
 @Configuration
@@ -29,5 +32,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
     registry.addHandler(lspWebSocketHandler, "/lsp").setAllowedOrigins("*");
     registry.addHandler(oclLspWebSocketHandler, "/ocl-lsp").setAllowedOrigins("*");
+  }
+
+  @Bean
+  public ServletServerContainerFactoryBean createWebSocketContainer(
+      @Value("${websocket.max-buffer-size:262144}") int maxBufferSize) {
+    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+    container.setMaxTextMessageBufferSize(maxBufferSize);
+    container.setMaxBinaryMessageBufferSize(maxBufferSize);
+    return container;
   }
 }
